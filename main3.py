522dcc1eea9b009a3f4daeca4a3b23524d660a80
misha <99326573+Dai-tenshi@users.noreply.github.com>
2022-02-18 09:38:53 +0400

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,6 @@
+import random
+A=[random.randint(-25, 25)for i in range(-25, 25)]
+print("изначальный массив = ", A)
+A=sorted(A)
+print("отсортированный массив = ", A)
+print("для закрытия нажмите enter ")
