d5cd2f26b72b3e4d91fd5c78aeebdc131940b311
misha <99326573+Dai-tenshi@users.noreply.github.com>
2022-02-18 09:17:26 +0400

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import random
-A=[random.randint(-25, 25)for i in range(-25, 25)]
-print("изначальный массив = ", A)
-A=sorted(A)
-print("отсортированный массив = ", A)
-print(для закрытия нажмите enter)
-
